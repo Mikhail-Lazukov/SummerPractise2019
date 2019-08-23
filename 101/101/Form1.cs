1edbc061a6361bb8c0b3904401b1aafe26a2760b
misha <misha130200@gmail.com>
2019-08-23 17:09:40 +0700

    Card pictures was added

--- 101/101/Form1.cs
+++ 101/101/Form1.cs
@@ -15,7 +15,18 @@ namespace _101
         public Form1()
         {
             InitializeComponent();
+            PictureBox pb = new PictureBox();
+            pb.Location = new Point(0, 0);
+            GameLogic gl = new GameLogic();
+            gl.FillOriginalDeck();
+            pb.Size = new Size(100, 150);
+            pb.Image = gl.OriginalDeck.GetCard(35).FrontImage;
+            pb.Visible = true;
+            pb.Name = "pb";
+            pb.SizeMode = PictureBoxSizeMode.StretchImage;
+            this.Controls.Add(pb);
         }
 
+        
     }
 }
--- 101/101/GameLogic.cs
+++ 101/101/GameLogic.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 
 namespace _101
 {
     class GameLogic
     {
+        public Deck OriginalDeck = new Deck();
+        public void FillOriginalDeck ()
+        {
+            for (int i = 0; i < 36; i++)
+            {
+                Card card = new Card();
+                card.Dignity =  i/4  + 2;
+                if (i > 11) card.Dignity++;
+                if (card.Dignity == 9) card.Dignity = 0;
+                card.Suit = i % 4;
+                card.BackImage = Properties.Resources.back;
+                string imageName = String.Format("_{0}", i);
+                card.FrontImage = (Image)Properties.Resources.ResourceManager.GetObject(imageName);
+                OriginalDeck.AddCard(card);
+            }
+        }
+        
     }
 }
